def3bd117eccb15868cbab18186965d062634094
lucaperini <luca.perini@gmail.com>
2022-02-22 18:05:35 +0100

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 const km_number = parseInt(prompt("Quanti kilometri devi percorrere?"));
-if (isNaN (km_number)){
-    // console.log("Devi inserire un numero valido!");
-};
-let fair_price = 0.21 * km_number;
-// console.log("il prezzo del biglietto è: "+ fair_price);
+document.getElementById('total-km').innerHTML= km_number;
 const user_age=parseInt(prompt("Quanti anni hai?"));
+document.getElementById('passenger-age').innerHTML=user_age;
+let fair_price = 0.21 * km_number;
 let discount_18 = (fair_price / 100) * 20;
 let discount_65 = (fair_price / 100) * 40;
-let total_price ="";
+let total_price = fair_price;
+document.getElementById('total').innerHTML=total_price.toFixed(2) +"€";
 if (user_age < 18){
     total_price = fair_price - discount_18;
-    console.log("Sconto del 20% per chi è minorenne: "+ total_price);
+    document.getElementById('total-discount').innerHTML="Sconto del 20% per chi è minorenne. Il costo del tuo biglietto è "+ total_price.toFixed(2) +"€";
 } else if (user_age > 65) {
     total_price = fair_price - discount_65;
-    console.log("Sconto del 40% per gli over 65: "+ total_price);
+    document.getElementById('total-discount').innerHTML="Sconto del 40% per chi è over 65. Il costo del tuo biglietto è: "+ total_price.toFixed(2) +"€";
     
 } else {
     total_price=fair_price;
-    console.log("Il prezzo del tuo biglietto è: "+ total_price);
-    
+    document.getElementById('total').innerHTML=total_price.toFixed(2) +"€";
 }
 
-document.getElementById("total-km").innerHTML=km_number;
-document.getElementById()
+
+
+
+
 
 
 
